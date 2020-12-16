76270d12a65f05748b65711887812ae04e6a5430
Hendry Rodriguez <hendry.rodriguez@capitalblockchain.io>
2020-12-16 15:24:06 -0600

    the last number

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,19 @@
 
 pub fn store(conn: &mut mysql::Conn, wif: String, addr: String){
+    if !conn.ping() {
+        conn.reset().unwrap();
+    }
+    
 	conn.prep_exec("INSERT INTO address_rust (wif, addr) VALUES (?, ?)", (&wif, &addr)).unwrap();
 }
 
 pub fn exist_address(conn: &mut mysql::Conn, address: String) -> bool {
 	use std::borrow::Borrow;
+
+    if !conn.ping() {
+        conn.reset().unwrap();
+    }
+
     let mut result= conn.prep_exec("SELECT id FROM rich_list where address = ?", (&address, )).unwrap();
 
     match result.next() {
